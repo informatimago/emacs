3816535a51bf7741bc9e58721cfe8766ffc239a8
Pascal J. Bourguignon <pjb@informatimago.com>
2013-04-03 12:33:05 +0200

    Removed obsolete pjb-defclass, moved pjb-grep-here away.

--- pjb-sources.el
+++ pjb-sources.el
@@ -1410,74 +1410,6 @@ NOTE:           For now, no pretty-printing is done.
     (unescape-sharp)))
 
 
-
-;; ------------------------------------------------------------------------
-;; pjb-defclass
-;; ------------------------------------------------------------------------
-;; Syntactic sugar for defclass
-;;
-
-;; (defmacro pjb-attrib (name type &rest args)
-;;   "
-;; This macro outputs an attribute s-exp as used in defclass.
-;; ARGS  may be of length 1 or 2.
-;;       If (LENGTH ARGS) = 1 
-;;       then if the argument is a string, 
-;;            then it's taken as the documentation and the initial value is NIL
-;;            else it's taken as the initial value and the documentation is NIL.
-;;       else the first is the initial value and the second is the documentation.
-;; The initarg an accessor are the same keyword built from the name.
-;; "
-;;   (let ((iarg (intern (format ":%s" name)))
-;;         init doc)
-;;     (cond 
-;;       ((= 2 (length args))
-;;        (setq init (car  args)
-;;              doc  (cadr args)) )
-;;       ((= 1 (length args))
-;;        (if (stringp (car args))
-;;            (setq init nil
-;;                  doc  (car args))
-;;            (setq init (car args)
-;;                  doc  nil)) )
-;;       (t (error "Invalid arguments to pjb-attrib.")))
-;;     (if (and (symbolp type) (null init))
-;;         (setq type (list 'or 'null type)))
-;;     (if (null doc)
-;;         (setq doc (symbol-name name)))
-;;     `(,name 
-;;       :initform ,init 
-;;       :initarg  ,iarg
-;;       :accessor ,name
-;;       :type     ,type
-;;       :documentation ,doc)
-;;     )) ;;pjb-attrib
-
-
-(defmacro pjb-defclass (name super &rest args)
-  "
-This macro encapsulate DEFCLASS and allow the declaration of the attributes
-in a shorter syntax.
-ARGS  is a list of s-expr, whose car is either :ATT (to declare an attribute)
-      or :DOC to give the documentation string of the class.
-      (:OPT is not implemented yet).
-      See PJB-ATTRIB for the syntax of the attribute declation.
-      (:ATT name type [ init-value [doc-string] | doc-string ] )
-"
-  (let ((fields  nil)
-        (options nil))
-    (while args
-      (cond ((eq :att (caar args))
-             (push (macroexpand (cons 'pjb-attrib (cdar args))) fields))
-            ((eq :doc (caar args))
-             (push (cons :documentation (cdar args)) options))
-            )
-      (setq args (cdr args)))
-    (setq fields (nreverse fields))
-    (setq options (nreverse options))
-    `(defclass ,name ,super ,fields ,options)))
-
-
 ;; ------------------------------------------------------------------------
 ;; karnaugh & karnaugh-solve
 ;; ------------------------------------------------------------------------
@@ -2668,18 +2600,6 @@ silent:     When non-nil, don't issue any message whent the file type can't
     
     
 
-
-(defun pjb-grep-here (pattern)
-  "Does an egrep  in the current directory just asking for a pattern."
-  (interactive "segrep pattern: ")
-  (if (null pattern)
-      (error "Expecting a pattern to do the egrep."))
-  (if (string-equal "" pattern)
-      (error "The empty string matches everything. Are you happy?"))
-  (grep (format "egrep -n -e '%s' `find . -type f -print` /dev/null" pattern)))
-
-
-
 ;;    We need to parse C arguments.
 ;; 
 ;;    We may have string or character literals (in which we must ignore
@@ -3461,7 +3381,7 @@ the FUNCTION can take."
       (set-shadow-map (list (cons (format "%s/" directory)
                                   (format "%s%s/" (file-name-directory directory) *shadow-directory-name*)))))
     (setf grep-find-command
-          (format "find %s \\( \\( -name build -o -name debug -o -name release -o -name .svn \\) -prune \\) -o -type f  \\(  -name \\*.h -o -name \\*.m -o -name \\*.mm -o -name \\*.c -name \\*.hh -o -name \\*.hxx -o -name \\*.cc  -o -name \\*.cxx -o -name \\*.lisp -o -name \\*.rb -o -name \\*.logs \\) -print0 | xargs -0 grep -niH -e "
+          (format "find %s \\( \\( -name build -o -name debug -o -name release -o -name .svn \\) -prune \\) -o -type f  \\(  -name \\*.xib -o -name \\*.h -o -name \\*.m -o -name \\*.mm -o -name \\*.c -name \\*.hh -o -name \\*.hxx -o -name \\*.cc  -o -name \\*.cxx -o -name \\*.lisp -o -name \\*.rb -o -name \\*.logs \\) -print0 | xargs -0 grep -niH -e "
                   *sources*)
           grep-host-defaults-alist nil)))
 
