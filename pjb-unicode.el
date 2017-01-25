ae9c154dc2514ebb34b186c25ced4290e50a3bb6
Pascal J. Bourguignon <pjb@informatimago.com>
2017-01-25 12:02:35 +0100

    Deleted trailing whitespaces.

--- pjb-unicode.el
+++ pjb-unicode.el
@@ -5,9 +5,9 @@
 ;;;;SYSTEM:             POSIX
 ;;;;USER-INTERFACE:     NONE
 ;;;;DESCRIPTION
-;;;;    
+;;;;
 ;;;;    Implements functions binding locally keys to unicode alphabets.
-;;;;    
+;;;;
 ;;;;AUTHORS
 ;;;;    <PJB> Pascal J. Bourguignon <pjb@informatimago.com>
 ;;;;MODIFICATIONS
@@ -15,19 +15,19 @@
 ;;;;BUGS
 ;;;;LEGAL
 ;;;;    AGPL3
-;;;;    
+;;;;
 ;;;;    Copyright Pascal J. Bourguignon 2017 - 2017
-;;;;    
+;;;;
 ;;;;    This program is free software: you can redistribute it and/or modify
 ;;;;    it under the terms of the GNU Affero General Public License as published by
 ;;;;    the Free Software Foundation, either version 3 of the License, or
 ;;;;    (at your option) any later version.
-;;;;    
+;;;;
 ;;;;    This program is distributed in the hope that it will be useful,
 ;;;;    but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;;;;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;;;;    GNU Affero General Public License for more details.
-;;;;    
+;;;;
 ;;;;    You should have received a copy of the GNU Affero General Public License
 ;;;;    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ;;;;**************************************************************************
@@ -81,7 +81,7 @@
   (loop for ch from ?A to ?Z
         for mf from #x1d56c
         do (local-set-key (kbd (string ch))          (string mf))
-           (local-set-key (kbd (string (+ #x20 ch))) (string (+ 26 mf))))) 
+           (local-set-key (kbd (string (+ #x20 ch))) (string (+ 26 mf)))))
 
 (defun sans-serif ()
   (interactive)
