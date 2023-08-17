8d19a7498a6cba4f5ba1f3b9d9efee37057a6c94
Pascal J. Bourguignon <pjb@informatimago.com>
2023-08-17 10:00:51 +0200

    Corrected save-region-to-termbin interactive form.

--- pjb-termbin.el
+++ pjb-termbin.el
@@ -5,9 +5,9 @@
 ;;;;SYSTEM:             POSIX
 ;;;;USER-INTERFACE:     NONE
 ;;;;DESCRIPTION
-;;;;    
+;;;;
 ;;;;    Save buffer or region to termbin.
-;;;;    
+;;;;
 ;;;;AUTHORS
 ;;;;    <PJB> Pascal J. Bourguignon <pjb@informatimago.com>
 ;;;;MODIFICATIONS
@@ -15,19 +15,19 @@
 ;;;;BUGS
 ;;;;LEGAL
 ;;;;    AGPL3
-;;;;    
+;;;;
 ;;;;    Copyright Pascal J. Bourguignon 2023 - 2023
-;;;;    
+;;;;
 ;;;;    This program is free software: you can redistribute it and/or modify
 ;;;;    it under the terms of the GNU Affero General Public License as published by
 ;;;;    the Free Software Foundation, either version 3 of the License, or
 ;;;;    (at your option) any later version.
-;;;;    
+;;;;
 ;;;;    This program is distributed in the hope that it will be useful,
 ;;;;    but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;;;;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;;;;    GNU Affero General Public License for more details.
-;;;;    
+;;;;
 ;;;;    You should have received a copy of the GNU Affero General Public License
 ;;;;    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ;;;;**************************************************************************
@@ -37,7 +37,7 @@
   "Save the current region by sending it using nc termbin.com port 9999,
 using a temporary output buffer, and return the string returned.
 The result is also put in the kill ring for easy pasting."
-  (interactive "p")
+  (interactive "r")
   (let ((output-buffer (generate-new-buffer " *temp*")))
     (call-process-region start end "nc" nil output-buffer nil "termbin.com" "9999")
     (let ((result (string-trim  (remove-if (lambda (ch) (find ch [13 0]))
