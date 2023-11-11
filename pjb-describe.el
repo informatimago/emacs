7c1f21e3405a5fb9b8715bdf7e37f05ddd2ccd94
Pascal J. Bourguignon <pjb@informatimago.com>
2023-11-11 15:19:06 +0100

    deleted trailing whitespaces.

--- pjb-describe.el
+++ pjb-describe.el
@@ -5,9 +5,9 @@
 ;;;;SYSTEM:             POSIX
 ;;;;USER-INTERFACE:     NONE
 ;;;;DESCRIPTION
-;;;;    
+;;;;
 ;;;;    describe
-;;;;    
+;;;;
 ;;;;AUTHORS
 ;;;;    <PJB> Pascal J. Bourguignon <pjb@informatimago.com>
 ;;;;MODIFICATIONS
@@ -15,19 +15,19 @@
 ;;;;BUGS
 ;;;;LEGAL
 ;;;;    AGPL3
-;;;;    
+;;;;
 ;;;;    Copyright Pascal J. Bourguignon 2023 - 2023
-;;;;    
+;;;;
 ;;;;    This program is free software: you can redistribute it and/or modify
 ;;;;    it under the terms of the GNU Affero General Public License as published by
 ;;;;    the Free Software Foundation, either version 3 of the License, or
 ;;;;    (at your option) any later version.
-;;;;    
+;;;;
 ;;;;    This program is distributed in the hope that it will be useful,
 ;;;;    but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;;;;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;;;;    GNU Affero General Public License for more details.
-;;;;    
+;;;;
 ;;;;    You should have received a copy of the GNU Affero General Public License
 ;;;;    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ;;;;**************************************************************************
@@ -46,7 +46,7 @@
     (char-table t describe-buffer-case-table)
     (buffer t describe-categories)
     ;; (integer t describe-char)
-    
+
     (symbol charsetp        describe-character-set)
     (symbol coding-system-p describe-coding-system)
     ;; (symbol t describe-copying)
@@ -58,7 +58,7 @@
     (symbol facep describe-face)
     (symbol fontp describe-font)
     (symbol fontp  describe-fontset)
-    ((or symbol function) functionp describe-function) 
+    ((or symbol function) functionp describe-function)
     (string input-method-p describe-input-method)
     (string t describe-key-briefly)
     (string t describe-language-environment)
